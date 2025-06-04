5b7ba4098eab6e6d473815fbc8f41b03cd8392b4
@designervenkat <murty.av@gmail.com>
2025-06-04 13:22:42 +0000

    Tailwind Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
    return (
-      <div>
-         <h1>hello</h1>
+      <div className='bg-slate-800 h-screen w-screen grid place-content-center'>
+         <div className='flex items-center justify-center gap-x-2'>
+            <h1 className='text-base font-medium text-slate-400'>
+               Hello React!
+            </h1>
+            <img
+               src='/react.svg'
+               alt=''
+               width={20}
+               height={20}
+            />
+         </div>
       </div>
    )
 }
